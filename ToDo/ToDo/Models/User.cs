cbd786cfc8a52206dce0275825a137e4069c3ec1
TheOneTrueSpin <lspinelli@thespincorp.com>
2024-08-12 14:59:23 +0930

    added models and services

---
+++ ToDo/ToDo/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo.Models
+{
+    public class User
+    {
+        public string Username { get; set; } = "Defualt";
+        public string Password { get; set; } = "Defualt";
+
+    }
+}
