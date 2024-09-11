e67eff3162c30b28bf7f91ba1dc7c6702ae10604
TheOneTrueSpin <lspinelli@thespincorp.com>
2024-09-11 13:26:00 +0930

    Worked on list management service, yet to finish itemID's

--- ToDo/ToDo/Services/ListManagementService.cs
+++ ToDo/ToDo/Services/ListManagementService.cs
@@ -11,12 +11,35 @@ namespace ToDo.Services
     public class ListManagementService
     {
         private readonly AppData _appData;
-        public ListManagementService(AppData appData)
+        private readonly UserManagerService _userManagerService;
+        public ListManagementService(AppData appData, UserManagerService userManagerService)
         {
             _appData = appData;
+            _userManagerService = userManagerService;
         }
         public void AddItem(string input)
         {
+            int? currentUserId = _appData.CurrentUserId;
+
+            if (currentUserId is null)
+            {
+                throw new NullReferenceException("damn it, looks like the user is null :)");
+            }
+
+            User? user = _userManagerService.GetUserById((int)currentUserId);
+
+            if (user is null)
+            {
+                throw new NullReferenceException("damn it, looks like the user is null :)");
+            }
+            //IDs need to be worked out, currently no ID for to do items 
+            ToDoItem toDoItem = new ToDoItem()
+            {
+                ToDoTitle = input,
+                IsCompleted = false
+            };
+
+            user.ToDoItems.Add(toDoItem);
 
         }
 
@@ -40,9 +63,28 @@ namespace ToDo.Services
 
         }
 
-        private ToDoItem? GetItemById(int id) 
+        private ToDoItem? GetItemById(int toDoItemId) 
         {
-            throw new NotImplementedException();
+            int? currentUserId = _appData.CurrentUserId;
+
+            if (currentUserId is null)
+            {
+                throw new NullReferenceException("damn it, looks like the user is null :)");
+            }
+
+            User? user = _userManagerService.GetUserById((int)currentUserId);
+            if (user is null)
+            {
+                throw new NullReferenceException("damn it, looks like the user is null");
+            }
+            for (int i = 0; i < user.ToDoItems.Count; i++)
+            {
+                if (toDoItemId == user.ToDoItems[i].Id)
+                {
+                    return user.ToDoItems[i];
+                }
+            }
+            return null;
         }
 
 
